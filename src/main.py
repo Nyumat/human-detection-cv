5132d37d62b1dcf3a0aa126d0b3f93b224b28726
thomas <46255836+Nyumat@users.noreply.github.com>
2021-04-21 15:26:15 -0400

    ++arg parser function ++webcam detection

--- src/main.py
+++ src/main.py
@@ -4,16 +4,8 @@ import numpy
 import imutils # A library I discovered the other day that has helpful methods for working with opencv and numpy.
 import argparse # To be used for testing through command line
 
-# A pre-trained model available to us for detectcting humans; built into OpenCV!
-model = cv2.HOGDescriptor()
-# Will feed support vector machine with this call to the model.
-model.setSVMDetector(cv2.HOGDescriptorr_getDefaultPeopleDetector())
-
-# I'm still up in arms on how to implement this correctly, but I DO know we need a few things.
-
 """
 • A way to detect a person in singular frame.
-• I want to add personal webcam capability so I'll need a function to process that logic.
 • This entire thing will be tested through the command line so the argparse lib will come in handy for checking the values in the dict it returns.
 • Some sort of way to track the amount of 'humans' that've been processed. Probably will just be a plain old counter but yeah.
 """
@@ -31,3 +23,40 @@ def render_human(frame):
       cv2.putText(frame, f"Humans Seen : {human_count-1}", (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
       cv2.imshow('Results',frame)
       return frame
+
+def output_option(args):
+      path_to_video = args['video']
+      path_to_image = args['image']
+
+      if str(args["camera"]) == 'true':
+            use_camera = True
+      else:
+            use_camera = False
+      dis = None 
+      if args['output'] is not None and path_to_image is None:
+            dis = cv2.VideoWriter(args['output'], cv2.VideoWriter_fourcc(*'MJPG'), 10, (600,600))
+
+      if use_camera:
+            detectByCamera()
+
+def detectByCamera(dis):
+      output = cv2.VideoCapture(0)
+      print("Detecting Humans...")
+      while True:
+            inp,frame = output.read()
+            frame = render_human(frame)
+            if dis is not None:
+                  dis.write(frame)
+            trigger = cv2.waitKey(1)
+            if trigger == ord('q'):
+                  break
+      output.release()
+      cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    # A pre-trained model available to us for detectcting humans; built into OpenCV!
+    model = cv2.HOGDescriptor()
+    # Will feed support vector machine with this call to the model.
+    model.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    args = argsParser()
+    output_option(args)
